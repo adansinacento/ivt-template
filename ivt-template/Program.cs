23e9fa6a2fd919d9f3ed4d6dddfc6fb2e88a6873
Adan Sandoval <adansinacento@gmail.com>
2022-03-02 20:50:55 -0600

    Initial commit

---
+++ ivt-template/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ivt_template
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * ¡Hey! Recuerda que esto no se toca, escribe tu código en el archivo llamado WorkHere.cs
+             */
+            new WorkHere().Main();
+        }
+    }
+}
---
+++ ivt-template/WorkHere.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace ivt_template
+{
+    public class WorkHere
+    {
+        public void Main()
+        {
+            //Write your code after this line
+
+            Console.WriteLine("Hello world!");
+            return;
+        }
+    }
+}
